446605ae64b044eb8b39f7fad227db213a5d2d4a
Daniel Stocks <daniel@webcloud.se>
2010-09-17 10:57:53 +0200

    temporary IE onload issue

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -80,7 +80,7 @@
             // On page refresh, IE doesn't re-populate user input
             // until the window.onload event is fired.
             if ($.browser.msie) {
-                $(window).load(function() {
+                $(window).load(function() {
                     if(input.val()) {
                         input.removeClass("placeholder");
                     }
